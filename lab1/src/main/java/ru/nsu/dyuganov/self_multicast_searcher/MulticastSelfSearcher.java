758293acc809f7aca250e0b5f5c3fad1bb2ef9a6
dyuganov-n <dyuganov.n@gmail.com>
2021-09-23 21:45:22 +0700

    work ing progress

--- lab1/src/main/java/ru/nsu/dyuganov/self_multicast_searcher/MulticastSelfSearcher.java
+++ lab1/src/main/java/ru/nsu/dyuganov/self_multicast_searcher/MulticastSelfSearcher.java
@@ -10,31 +10,31 @@ import java.util.concurrent.TimeUnit;
 
 public class MulticastSelfSearcher {
     private final MulticastSocket multicastSocket = new MulticastSocket(Constants.port);
-    private InetAddress groupInetAddress;
+    private final InetAddress groupInetAddress;
 
     private final UUID thisUUID = UUID.randomUUID();
-    byte[] sendMessageBytes = thisUUID.toString().getBytes();
-    private final byte[] receiveMessageBytes = new byte[sendMessageBytes.length];
-    private final DatagramPacket sendMessagePacket = new DatagramPacket(sendMessageBytes, sendMessageBytes.length, groupInetAddress, Constants.port);
-    private DatagramPacket receiveMessagePacket = new DatagramPacket(receiveMessageBytes, receiveMessageBytes.length, groupInetAddress, Constants.port);
+    private final byte[] sendBytes = thisUUID.toString().getBytes();
+    private final byte[] receiveBytes = new byte[sendBytes.length];
+    private final DatagramPacket sendMessagePacket;
+    private DatagramPacket receiveMessagePacket;
 
     private final HashMap<UUID, String> uuidToIpCopies = new HashMap<>();
+    private final HashMap<UUID, Long> uuidToLastMessageTimeoutMills = new HashMap<>();
 
     public MulticastSelfSearcher(String multicastGroupIp) throws IOException {
         groupInetAddress = InetAddress.getByName(multicastGroupIp);
         if (!groupInetAddress.isMulticastAddress()) {
             throw new IllegalArgumentException("ip " + multicastGroupIp + " is not multicast address");
         }
+        sendMessagePacket = new DatagramPacket(sendBytes, sendBytes.length, groupInetAddress, Constants.port);
+        receiveMessagePacket = new DatagramPacket(receiveBytes, receiveBytes.length, groupInetAddress, Constants.port);
         multicastSocket.joinGroup(groupInetAddress);
         multicastSocket.setSoTimeout(Constants.disconnectTimeoutMills);
     }
 
     void findProgCopies() throws IOException {
-        if (!multicastSocket.isConnected()) { // ЗДЕСЬ НЕ ПРОХОДИТ ПРОВЕРКУ
-            System.err.println("multicastSocket is not connected");
-            return;
-        }
         while (true) {
+            //uuidToIpCopies.clear();
             multicastSocket.send(sendMessagePacket);
             receiveMessage();
             updateCopiesList();
@@ -56,14 +56,64 @@ public class MulticastSelfSearcher {
             System.err.println("Cannot update copies list. Receive message packet (or its components) is null.");
             return;
         }
-        addCopyId();
+        //addCopyId();
+        addLazyCopyId();
     }
 
     private void addCopyId() {
-        UUID messageUUID = UUID.fromString(new String(receiveMessagePacket.getData()));
-        if (!messageUUID.equals(thisUUID)) {
-            String messageSenderIp = receiveMessagePacket.getAddress().getHostAddress();
-            uuidToIpCopies.put(messageUUID, messageSenderIp);
+        UUID otherUUID = UUID.fromString(new String(receiveMessagePacket.getData()));
+        if (otherUUID.equals(thisUUID)) {
+            return;
+        }
+        String messageSenderIp = receiveMessagePacket.getAddress().getHostAddress();
+        //uuidToIpCopies.put(otherUUID, messageSenderIp);
+        long currentTime = System.currentTimeMillis();
+        if(!uuidToIpCopies.containsKey(otherUUID)){
+            uuidToIpCopies.put(otherUUID, messageSenderIp);
+            uuidToLastMessageTimeoutMills.put(otherUUID, currentTime);
+            return;
+        }
+
+/*        long lastMsgTimeGapMills = currentTime - uuidToLastMessageTimeoutMills.get(otherUUID);
+        if(Constants.clearUUIDListTimeoutMills < lastMsgTimeGapMills){
+            uuidToLastMessageTimeoutMills.remove(otherUUID);
+            uuidToIpCopies.remove(otherUUID);
+            return;
+        }*/
+        uuidToLastMessageTimeoutMills.put(otherUUID, currentTime);
+        checkForTimeout();
+    }
+
+    private void addLazyCopyId() {
+        UUID otherUUID = UUID.fromString(new String(receiveMessagePacket.getData()));
+        if (otherUUID.equals(thisUUID)) {
+            return;
+        }
+        String messageSenderIp = receiveMessagePacket.getAddress().getHostAddress();
+
+        uuidToIpCopies.put(otherUUID, messageSenderIp);
+        checkForLazyTimeout();
+    }
+
+    private void checkForTimeout(){
+        long currentTimeMills = System.currentTimeMillis();
+        for(var item : uuidToLastMessageTimeoutMills.entrySet()){
+            long lastMsgTimeGapMills = currentTimeMills - item.getValue();
+            if(Constants.clearUUIDListTimeoutMills > lastMsgTimeGapMills){
+                UUID keyToRemove = item.getKey();
+                uuidToIpCopies.remove(keyToRemove);
+                uuidToLastMessageTimeoutMills.remove(keyToRemove);
+            }
+        }
+    }
+
+    private long lastTimeCheck = System.currentTimeMillis();
+    private void checkForLazyTimeout(){
+        long currTimeMills = System.currentTimeMillis();
+        long timeGap = currTimeMills - lastTimeCheck;
+        if(Constants.clearUUIDListTimeoutMills > timeGap){
+            uuidToIpCopies.clear();
+            lastTimeCheck = currTimeMills;
         }
     }
 
