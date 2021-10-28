3ca64aaee8a8057fe9d4ff29c6d946490a889fd7
dyuganov-n <dyuganov.n@gmail.com>
2021-10-28 12:18:42 +0700

    fix

--- lab2/Server/src/main/java/ru/nsu/dyuganov/file_sender_server/Server.java
+++ lab2/Server/src/main/java/ru/nsu/dyuganov/file_sender_server/Server.java
@@ -1,11 +1,14 @@
 package ru.nsu.dyuganov.file_sender_server;
 
+import lombok.NonNull;
 import lombok.SneakyThrows;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -16,22 +19,22 @@ public class Server {
     private final String UPLOADS_DIRECTORY;
     private final ServerSocket serverSocket;
 
-    Server(int port) throws IOException {
-        final Properties properties = new Properties();
-        properties.load(Main.class.getClassLoader().getResourceAsStream(Main.PROPERTIES_FILE_NAME));
+    @SneakyThrows
+    Server(int port) {
+        @NonNull final Configuration config = new Configurations().properties(new File(Main.PROPERTIES_FILE_NAME));
         logger.debug("Properties file \"" + Main.PROPERTIES_FILE_NAME + "\" loaded");
 
-        final int MIN_PORT = Integer.parseInt(properties.getProperty("MIN_PORT"));
-        final int MAX_PORT = Integer.parseInt(properties.getProperty("MAX_PORT"));
+        final int MIN_PORT = config.getInt("MIN_PORT");
+        final int MAX_PORT = config.getInt("MAX_PORT");
         if (MIN_PORT > port || MAX_PORT < port) {
             throw new IllegalArgumentException("Port is not correct");
         }
         serverSocket = new ServerSocket(port);
 
-        final int THREAD_POOL_SIZE = Integer.parseInt(properties.getProperty("THREAD_POOL_SIZE"));
+        final int THREAD_POOL_SIZE = config.getInt("THREAD_POOL_SIZE", 1);
         threadPool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
 
-        UPLOADS_DIRECTORY = properties.getProperty("UPLOADS_DIRECTORY");
+        UPLOADS_DIRECTORY = config.getString("UPLOADS_DIRECTORY", "/uploads");
     }
 
     @SneakyThrows
